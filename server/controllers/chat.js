c4cadb97cdf2e589a74a005a1746f1bceba903e1
amish <pithvaamish76@gmail.com>
2024-03-14 02:20:44 +0530

    in Chat API - NewChat + getMyChats + getMyGroups done

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,5 +1,6 @@
 const { TryCatch, ErrorHandler } = require("../utils/ErrorHandler");
 const { Chat } = require("../models/chat");
+const { getOtherMember } = require("../lib/helper");
 const { emitEvent } = require("../utils/feature");
 const { ALERT, REFETCH_CHAT } = require("../constants/events");
 
@@ -10,11 +11,11 @@ const newGroupChat = TryCatch(async (req, res, next) => {
     return next(new ErrorHandler("Minimum 2 members are required", 400));
   }
 
-  const allMembers = [...members, req.user];
+  const allMembers = [...members, req.userId]; // add yourself into group
   await Chat.create({
     name,
     groupChat: true,
-    creator: req.user,
+    creator: req.userId,
     members: allMembers,
   });
 
@@ -27,4 +28,51 @@ const newGroupChat = TryCatch(async (req, res, next) => {
   });
 });
 
-module.exports = { newGroupChat };
+const getMyChats = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({
+    members: req.userId,
+  }).populate("members", "name avatar");
+
+  // return res.status(200).json({ success: true, chats });
+
+  // we can also use aggregation pipeline here
+  const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+    const otherMember = getOtherMember(members, req.userId);
+    return {
+      _id,
+      groupChat,
+      avatar: groupChat
+        ? members.slice(0, 3).map(({ avatar }) => avatar?.url)
+        : [otherMember?.avatar?.url],
+      name: groupChat ? name : otherMember?.name,
+      members: members.reduce((prev, curr) => {
+        if (curr._id.toString() !== req.userId.toString()) {
+          prev.push(curr._id);
+        }
+        return prev;
+      }, []),
+    };
+  });
+  return res.status(200).json({ success: true, transformedChats });
+});
+
+const getMyGroups = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({
+    members: req.userId,
+    groupChat: true,
+    creator: req.userId,
+  }).populate("members", "name avatar");
+
+  const groups = chats.map(({ _id, name, members, groupChat }) => {
+    return {
+      _id,
+      groupChat,
+      name,
+      avatar: members.slice(0, 3).map(({ avatar }) => avatar?.url),
+    };
+  });
+
+  return res.status(200).json({ success: true, groups });
+});
+
+module.exports = { newGroupChat, getMyChats, getMyGroups };
--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middlewares/auth");
-const { newGroupChat } = require("../controllers/chat");
+const {
+  newGroupChat,
+  getMyChats,
+  getMyGroups,
+} = require("../controllers/chat");
 
 router.use(isAuthenticated);
 // router.
 router.post("/new", newGroupChat);
+router.get("/my", getMyChats);
+router.get("/my/groups", getMyGroups);
 module.exports = router;
